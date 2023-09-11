1e7ff2bf554c5eeb8f0424fe8716c23e10ca7874
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-09-11 23:57:38 +0600

    Functional car added

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,18 +1,15 @@
-import { useEffect, useState } from 'react';
+
 import './Bottle.css';
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handlePurchase}) => {
     const{id, img, price} = bottle;
-    // State declear
-    const[purchase, setPurchase]=useState([]);
-    // Use effect declear
     
     return (
         <div className="bottle">
             <h3>Bottle:{id}</h3>
             <img src={img} alt="" />
             <p>Price:${price}</p>
-            <button>Buy Now</button>
+            <button onClick={()=>handlePurchase(bottle)}>Buy Now</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -7,18 +7,30 @@ import './Bottles.css';
 
 const Bottles = () => {
     const[bottles, setBottles]= useState([]);
+    const [cart, setCart]= useState([]);
 
     useEffect(() => {
         fetch('bottles.json')
         .then(res => res.json()) 
         .then(data => setBottles(data))
 }, [])
+
+    const handlePurchase = (bottle) =>{
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    }
     return (
         <div>
-            <h2 className='app'>Items:{bottles.length}</h2>
+            <h2 className='app'>Bottle available:{bottles.length}</h2>
+            <h3>Cart:{cart.length}</h3>
             <div className="bottles">
                 {
-                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                    bottles.map(bottle =>
+                         <Bottle key={bottle.id} 
+                         bottle={bottle}
+                         handlePurchase={handlePurchase}
+                         >
+                         </Bottle>)
                 }
             </div>
         </div>
